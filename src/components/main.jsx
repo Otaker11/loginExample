fb11ebeec15e9d6524f841df25f26eb328164e00
Josue Cano <84486353+Otaker11@users.noreply.github.com>
2024-01-17 15:10:53 -0600

    1er Commit: Version1

---
+++ src/components/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Login from './Login.jsx'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+    <Login></Login>
+)
